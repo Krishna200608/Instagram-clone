c1548bd6209485ab76eebd243b9d30aa8609237c
Krishna <krishnasikheriya001@gmail.com>
2025-06-13 14:02:49 +0530

    changes made

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -1,8 +1,9 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
+    const databaseName = "instagram";
     try {
-        await mongoose.connect(process.env.MONGODB_URI);
+        await mongoose.connect(`${process.env.MONGODB_URI}/${databaseName}`);
         console.log('mongodb connected successfully.');
     } catch (error) {
         console.log(error);
